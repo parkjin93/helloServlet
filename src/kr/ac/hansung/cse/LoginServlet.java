e3a97caffb5c26618360a75bd2793f8c3477a914
parkjin93 <qkrwls1015@naver.com>
2019-10-06 17:34:03 +0900

    Create the first servlet project

---
+++ src/kr/ac/hansung/cse/LoginServlet.java
@@ -0,0 +1,44 @@
+package kr.ac.hansung.cse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/loginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		 
+		PrintWriter out = response.getWriter();
+		
+		String htmlresponse = "<html>";
+		htmlresponse += "<h2>your username is " + username + "<br/>";
+		htmlresponse += "<h2>your password is " + password + "<h2/>";
+		htmlresponse += "</html>";
+		
+		out.println(htmlresponse);
+	}
+
+}
